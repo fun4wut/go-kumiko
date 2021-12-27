27b19d949f582ed6dbb0b289e1dea7aeb73a0d20
fun4wut <nihouze@163.com>
2021-12-27 12:32:19 +0800

    feat: 路由组

--- kumiko/kumiko.go
+++ kumiko/kumiko.go
@@ -6,28 +6,56 @@ import (
 
 type HandlerFn func(ctx *Ctx)
 
+type RouterGroup struct {
+	middlewares []HandlerFn
+	prefix      string
+	parent      *RouterGroup
+	top         *Kumiko
+}
+
 type Kumiko struct {
-	*router
+	router       *router
+	*RouterGroup // 全局对象本身也有路由组能力
+	groups       []*RouterGroup
 }
 
 func New() *Kumiko {
-	return &Kumiko{
+	k := &Kumiko{
 		router: newRouter(),
 	}
+	k.RouterGroup = &RouterGroup{top: k}
+	k.groups = []*RouterGroup{k.RouterGroup}
+	return k
 }
 
-func (k *Kumiko) Get(path string, handler HandlerFn) {
-	k.addRoute("GET", path, handler)
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	top := g.top
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		top:    top,
+	}
+	top.groups = append(top.groups, newGroup)
+	return newGroup
 }
 
-func (k *Kumiko) Post(path string, handler HandlerFn) {
-	k.addRoute("POST", path, handler)
+// 在路由组上去定义路由方法，这样全局对象本身也可以享受
+func (g *RouterGroup) addRoute(method, path string, handler HandlerFn) {
+	g.top.router.addRoute(method, g.prefix+path, handler)
+}
+
+func (g *RouterGroup) Get(path string, handler HandlerFn) {
+	g.addRoute("GET", path, handler)
+}
+
+func (g *RouterGroup) Post(path string, handler HandlerFn) {
+	g.addRoute("POST", path, handler)
 }
 
 func (k *Kumiko) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	// 每次请求都构造一个上下文给handler使用
 	ctx := newCtx(writer, request)
-	k.handle(ctx)
+	k.router.handle(ctx)
 }
 
 func (k *Kumiko) Run(addr string) error {
--- main.go
+++ main.go
@@ -13,16 +13,19 @@ func main() {
 			"a": "2",
 		})
 	})
-	server.Get("/rua/:id/eqqq", func(ctx *kumiko.Ctx) {
-		v := func() string {
-			if v, ok := ctx.GetPathParam("id"); ok {
-				return v
-			} else {
-				return ""
-			}
-		}()
-		ctx.WriteText(http.StatusOK, "221333"+v)
-	})
+	{
+		g1 := server.Group("/rua")
+		g1.Get("/:id/faq", func(ctx *kumiko.Ctx) {
+			v := func() string {
+				if v, ok := ctx.GetPathParam("id"); ok {
+					return v
+				} else {
+					return ""
+				}
+			}()
+			ctx.WriteText(http.StatusOK, "221333"+v)
+		})
+	}
 
 	log.Fatal(server.Run("0.0.0.0:4001"))
 }
